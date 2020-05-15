438ae159ab6cfb0a53910610651d839d9ee70a75
alex <57417529+alexanderplotnikov@users.noreply.github.com>
2020-05-15 22:07:59 +0300

    create todo factory

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-const Todo = () => {
-    
-}
+const createNewProject = (title, description, dueDate, priority) => {
+    const prop = {
+        title,
+        description,
+        dueDate,
+        priority,
+        // getTitle: () => {return title} 
+    }
+    const getters = {
+        getTitle: () => {return prop.title},
+        getDesc: () => {return prop.description},
+        getDueDate: () => {return prop.dueDate},
+        getPriority: () => {return prop.priority},
+
+    }
+    return Object.assign({}, prop, getters); 
+};
+
+let obj = createNewProject("swe", "project 1 due tmrw", "10/06/2020", "high");
+console.log(obj);
+console.log(obj.getTitle());
